2bc22d05986d57acf1e579019145d7214db7f7f2
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-18 15:59:29 +0530

    :zap: :lipstick: event search design

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,8 +1,18 @@
 import Link from "next/link";
-import classes from './button.module.css'
+import classes from "./button.module.css";
 
-export default function Button({
-  children, href
-}) {
-  return <Link href={href} className={classes.btn}>{children}</Link>
+export default function Button({ children, href, onClick }) {
+  if (href) {
+    return (
+      <Link href={href} className={classes.btn}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button onClick={onClick} className={classes.btn}>
+      {children}
+    </button>
+  );
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,4 +1,5 @@
 import EventList from "@/components/events/event-list";
+import EventSearch from "@/components/events/event-search";
 import { getAllEvents } from "@/dummy-data"
 
 export default function EventsPage() {
@@ -6,6 +7,7 @@ export default function EventsPage() {
   const allEvents = getAllEvents();
 
   return <div>
+    <EventSearch />
     <EventList items={allEvents}/>
   </div>
 }
