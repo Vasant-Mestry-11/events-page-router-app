8d1dc9bceb1937f95adedf687654fb27a96b95a8
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-18 16:57:59 +0530

    :zap: :art: Filtered events logic implemented

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,5 +1,42 @@
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
+
 export default function FilteredEvents() {
-  return <div>
-    <h1>Filtered Events</h1>
-  </div>
+  const router = useRouter();
+  const { slug } = router.query;
+
+  if (!slug) {
+    return <p>Loading...</p>
+  }
+
+  const filteredYear = slug[0];
+  const filteredMonth = slug[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numMonth < 0 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter, Please search valid values</p>
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth
+  })
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found</p>
+  }
+
+  return (
+    <div>
+      <h1>Filtered Events</h1>
+    </div>
+  );
 }
