d259242a08ab1bb2f7180abc2943bf53aa442f0d
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-18 15:36:53 +0530

    :zap: :art: :lipstick: layout and main header added

---
+++ components/layout/layout.js
@@ -0,0 +1,13 @@
+import MainHeader from "./main-header";
+
+
+export default function Layout({
+  children
+}) {
+  return <>
+    <MainHeader />
+    <main>
+      {children}
+    </main>
+  </>
+}
