8cf6fba41b0d8a19f8928362f6169b42aab7b7ca
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-26 19:47:21 +0530

    :zap: :art: Added SSG to homepage, fetching data from :fire: firebase

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,17 @@
 import EventList from "@/components/events/event-list";
 import EventSearch from "@/components/events/event-search";
-import { getAllEvents } from "@/dummy-data"
+// import { getAllEvents } from "@/dummy-data"
 import { useRouter } from "next/router";
 
-export default function EventsPage() {
-
+export default function EventsPage({ events }) {
+  // console.log(events)
   const router = useRouter()
-  const allEvents = getAllEvents();
+
+  if (!events || events.length === 0) {
+    return <p>No events</p>;
+  }
+
+  // const allEvents = getAllEvents();
 
   function handleSearch(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -17,6 +22,32 @@ export default function EventsPage() {
 
   return <>
     <EventSearch onSearch={handleSearch} />
-    <EventList items={allEvents} />
+    <EventList items={events} />
   </>
+}
+
+
+export async function getServerSideProps() {
+  const response = await fetch('https://nextjs-events-page-router-default-rtdb.firebaseio.com/events.json')
+  const data = await response.json();
+
+  const transformedData = [];
+
+  for (let key in data) {
+    transformedData.push({
+      id: key,
+      title: data[key].title,
+      description: data[key].description,
+      location: data[key].location,
+      date: data[key].date,
+      image: data[key].image,
+      isFeatured: data[key].isFeatured,
+    })
+  }
+
+  return {
+    props: {
+      events: transformedData
+    }
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import EventList from "@/components/events/event-list";
-import { getFeaturedEvents } from "@/dummy-data";
+import { getFeaturedEvents } from "@/helpers/apiUtils";
 
-export default function HomePage() {
-  const featuredEvents = getFeaturedEvents();
-
+export default function HomePage({ events }) {
   return <div>
-    <EventList items={featuredEvents} />
+    <EventList items={events} />
   </div>
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents()
+  return {
+    props: {
+      events: featuredEvents,
+    }
+  }
 }
