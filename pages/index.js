112602e03a45ccf52d193a28e7005db03de6cfde
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-27 15:23:48 +0530

    :zap: :mag: added HEAD tag from nextjs to add head content

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,28 @@
 import EventList from "@/components/events/event-list";
 import { getFeaturedEvents } from "@/helpers/apiUtils";
+import Head from "next/head";
 
 export default function HomePage({ events }) {
-  return <div>
-    <EventList items={events} />
-  </div>
+  return (
+    <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you evolve."
+        />
+      </Head>
+      <EventList items={events} />
+    </div>
+  );
 }
 
 export async function getStaticProps() {
-  const featuredEvents = await getFeaturedEvents()
+  const featuredEvents = await getFeaturedEvents();
   return {
     props: {
       events: featuredEvents,
     },
-    revalidate: 1800
-  }
+    revalidate: 1800,
+  };
 }
