114cd39e60c2911679ef1a3aba6c3b1179204f71
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-27 12:45:06 +0530

    :zap: :art: events page optimization through getStaticProps

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,23 +1,18 @@
 import EventList from "@/components/events/event-list";
 import EventSearch from "@/components/events/event-search";
-// import { getAllEvents } from "@/dummy-data"
+import { getAllEvents } from "@/helpers/apiUtils";
 import { useRouter } from "next/router";
 
 export default function EventsPage({ events }) {
-  // console.log(events)
   const router = useRouter()
 
   if (!events || events.length === 0) {
     return <p>No events</p>;
   }
 
-  // const allEvents = getAllEvents();
-
   function handleSearch(year, month) {
     const fullPath = `/events/${year}/${month}`;
-
     router.push(fullPath);
-
   }
 
   return <>
@@ -27,27 +22,13 @@ export default function EventsPage({ events }) {
 }
 
 
-export async function getServerSideProps() {
-  const response = await fetch('https://nextjs-events-page-router-default-rtdb.firebaseio.com/events.json')
-  const data = await response.json();
-
-  const transformedData = [];
-
-  for (let key in data) {
-    transformedData.push({
-      id: key,
-      title: data[key].title,
-      description: data[key].description,
-      location: data[key].location,
-      date: data[key].date,
-      image: data[key].image,
-      isFeatured: data[key].isFeatured,
-    })
-  }
+export async function getStaticProps() {
+  const events = await getAllEvents()
 
   return {
     props: {
-      events: transformedData
-    }
+      events
+    },
+    revalidate: 60
   }
 }
