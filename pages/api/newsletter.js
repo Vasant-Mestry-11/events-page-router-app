84e1d4920e3d1b5a376def5032274809a050e81e
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-05-03 19:04:59 +0530

    :zap: :lock: loaded mongo url from env file

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,31 +1,31 @@
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default async function handler(req, res) {
-
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const { email } = req.body;
 
-    if (!email || !email.includes('@')) {
+    if (!email || !email.includes("@")) {
       res.status(422).json({
-        message: 'Invalid email address'
-      })
-      return
+        message: "Invalid email address",
+      });
+      return;
     }
 
-    const client = await MongoClient.connect(
-      'mongodb+srv://vasantmestry11:tCHMYNZdKhd52IfL@cluster0.fmz53zz.mongodb.net'
-    )
+    const client = await MongoClient.connect(process.env.MONGO_CONNECTION_URL);
 
-    const db = client.db('newsletter');
+    const db = client.db("newsletter");
 
-    await db.collection('emails').insertOne({
-      email
-    })
+    await db.collection("emails").insertOne({
+      email,
+    });
 
-    client.close()
+    client.close();
 
     return res.status(201).json({
-      message: 'Email added successfully',
-    })
+      message: "Email added successfully",
+    });
   }
 }
