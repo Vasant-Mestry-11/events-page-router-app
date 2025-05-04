4eed36b757103d53296afec23e702c48504ec68f
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-05-04 13:10:40 +0530

    :zap: :art: Error handling in newsletter

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -3,6 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(process.env.MONGO_CONNECTION_URL);
+  return client
+}
+
+async function insertDocument(client, document) {
+  const db = client.db("events");
+
+  return await db.collection("newsletter").insertOne(document);
+}
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { email } = req.body;
@@ -14,15 +25,26 @@ export default async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(process.env.MONGO_CONNECTION_URL);
+    let client;
 
-    const db = client.db("events");
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({
+        message: "Failed to connect to the database"
+      })
+      return;
+    }
 
-    await db.collection("newsletter").insertOne({
-      email,
-    });
-
-    client.close();
+    try {
+      await insertDocument(client, { email })
+      client.close();
+    } catch (error) {
+      res.status(500).json({
+        message: "Failed to insert the record"
+      })
+      return;
+    }
 
     return res.status(201).json({
       message: "Email added successfully",
