34119da8f290b9084522ee7842f8714cd8ea6a4e
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-06-05 21:02:35 +0530

    :zap: :lipstick: notification component

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,18 @@
 import Layout from "@/components/layout/layout";
 import "../styles/globals.css";
 import Head from "next/head";
+import Notification from "@/components/ui/notification";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
         <title>NextJS Events</title>
-        <meta name="description" content="NextJS events"/>
+        <meta name="description" content="NextJS events" />
         <meta name="viewport" content="intial-scale=1.0 width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification title="Test" message="This is test message" status="pending" />
     </Layout>
   );
 }
