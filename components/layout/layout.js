bce03f89c2ff4c1c8e5cdd0154f535d0fa5567e3
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-06-10 20:02:12 +0530

    :zap: :art: moved notification provider to layout component

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,13 +1,25 @@
+import { NotificationProvider } from "@/store/notification-context";
 import MainHeader from "./main-header";
+import { useContext } from "react";
+import Notification from "../ui/notification";
 
+export default function Layout({ children }) {
+  const { notification } = useContext(NotificationProvider);
+  const activeNotification = notification;
 
-export default function Layout({
-  children
-}) {
-  return <>
-    <MainHeader />
-    <main>
-      {children}
-    </main>
-  </>
+  return (
+    <>
+      <NotificationProvider>
+        <MainHeader />
+        <main>{children}</main>
+        {activeNotification && (
+          <Notification
+            title={activeNotification.title}
+            message={activeNotification.message}
+            status={activeNotification.status}
+          />
+        )}
+      </NotificationProvider>
+    </>
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,17 @@
 import Layout from "@/components/layout/layout";
 import "../styles/globals.css";
 import Head from "next/head";
-import Notification from "@/components/ui/notification";
-import { NotificationProvider } from "@/store/notification-context";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <NotificationProvider>
-      <Layout>
-        <Head>
-          <title>NextJS Events</title>
-          <meta name="description" content="NextJS events" />
-          <meta name="viewport" content="intial-scale=1.0 width=device-width" />
-        </Head>
-        <Component {...pageProps} />
-        <Notification title="Test" message="This is test message" status="pending" />
-      </Layout>
-    </NotificationProvider>
+    <Layout>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name="description" content="NextJS events" />
+        <meta name="viewport" content="intial-scale=1.0 width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
